b2fc18417dd9bd34043d2cdd8c149c66f0a021e6
unknown <ashishbhandari380@gmail.com>
2025-09-14 21:32:00 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='app'>
+      <Navbar />
       
-    </>
+    </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='Navbar'>
+     
     </div>
   )
 }
