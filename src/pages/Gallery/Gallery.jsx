45135b539a0f97629f444100086bb12dcea3ea3c
unknown <ashishbhandari380@gmail.com>
2025-09-17 22:45:51 +0545

    image page ni banauxu la aaba

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Gallery.css'
+import img from '../../assets/ol.png'
+import cartoon from '../../assets/cartoon-cnt.png'
+
 const Gallery = () => {
   return (
-    <div>
-      xaina photo aaba
+    <div className='gallery' id='gallery'>
+      <img src={img} alt="hii" />
+      <img src={cartoon} alt="hello" />
     </div>
   )
 }
